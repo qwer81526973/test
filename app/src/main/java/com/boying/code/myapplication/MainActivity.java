17e1b522ebec1e321f317a73b623e23693b8e307
qwer81526973 <liangbin_xy@163.com>
2019-10-10 13:46:36 +0800

    Revert "5"
    This reverts commit c297aeb7

--- app/src/main/java/com/boying/code/myapplication/MainActivity.java
+++ app/src/main/java/com/boying/code/myapplication/MainActivity.java
@@ -10,7 +10,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        // 11223
+        //5
         //6
     }
 }
